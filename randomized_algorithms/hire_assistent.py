96ce99ce630f92133c5cc307603e1aaaccee5d08
Anto Čabraja <cabraja.anto@gmail.com>
2013-11-16 16:07:19 +0100

    finished basic documentation and algorithm

---
+++ randomized_algorithms/hire_assistent.py
@@ -0,0 +1,18 @@
+def hire(h):
+	return h+1
+
+def hire_assistent():
+	best = 0
+	h = 0
+	A = []	
+	n = input()
+	for i in xrange(0,n):
+		A.append( input())
+	for i in xrange(0,n):
+		result = A[i];
+		if result > best:
+			h = hire(h);
+			best = result
+	return h
+
+print hire_assistent()
