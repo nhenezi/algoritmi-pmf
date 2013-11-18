de90fc01156abde1a7b0812e5030710c57341aa1
Anto Čabraja <cabraja.anto@gmail.com>
2013-11-18 21:39:54 +0100

    deleted tab

--- randomized_algorithms/hire_assistent.py
+++ randomized_algorithms/hire_assistent.py
@@ -1,18 +1,18 @@
 def hire(h):
-	return h+1
+    return h+1
 
 def hire_assistent():
-	best = 0
-	h = 0
-	A = []	
-	n = int(input())
-	for i in xrange(0,n):
-		A.append( int(input()))
-	for i in xrange(0,n):
-		result = A[i];
-		if result > best:
-			h = hire(h);
-			best = result
-	return h
+    best = 0
+    h = 0
+    A = []
+    n = int(input())
+    for i in xrange(0,n):
+        A.append( int(input()))
+    for i in xrange(0,n):
+        result = A[i];
+        if result > best:
+            h = hire(h);
+            best = result
+    return h
 
 print hire_assistent()
